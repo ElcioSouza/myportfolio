a03bdbb7366e25d26073318d5a776a9c064e88f5
Elcio de Souza Monico <elcio.monico@gmail.com>
2025-07-05 22:11:38 -0300

    whatsapp

--- src/components/WhatsAppButton/index.tsx
+++ src/components/WhatsAppButton/index.tsx
@@ -5,7 +5,7 @@ export default function WhatsAppButton() {
   return (
     <div>
       <a 
-        href="https://api.whatsapp.com/send?phone=5527997813275&amp;text=Olá Elcio, tudo bem? vi seu portifólio e estou interessado no seu trabalho."
+        href="https://api.whatsapp.com/send?phone=5527997813275&amp;text=Olá Elcio, tudo bem? vi seu portifólio e estou interessado no seu trabalho"
         className="fixed  m-2 w-full max-w-[60px] h-[60px] text-center text-3xl shadow-[2px_2px_3px_#999] z-[100] animate-[sm-shake-animation_linear_1.5s_infinite] rounded-[50px] right-2.5 bottom-2.5
   -webkit-animation: sm-shake-animation linear 1.5s infinite"
         target="_blank"
